4c5e810943a5076942424cadd4de3b9296e23c34
Xin Yu Zhang <xyzhang2022@gmail.com>
2024-03-28 09:50:27 -0400

    revised to have return statements

--- script-04.js
+++ script-04.js
@@ -2,14 +2,14 @@ let decision = prompt("Would you rather have chicken or pizza? ");
 
 function wyr(decision){
 	if (decision == "chicken"){
-		alert("Correct option");
+		return "Correct option";
 	}
 	else if (decision == "pizza"){
-		alert("Wrong option");
+		return "Wrong option";
 	}
 	else{
-		alert("You typed an invalid option silly");
+		return "You typed an invalid option silly";
 	}
 }
 
-wyr(decision);
+alert(wyr(decision));
